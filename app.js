4245d81975f85d0a36c10c9b4ca0a87e515f4b0f
mausooq <mauchoomausooq051@gmail.com>
2025-04-11 14:58:27 +0530

    done

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 connectDB()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
